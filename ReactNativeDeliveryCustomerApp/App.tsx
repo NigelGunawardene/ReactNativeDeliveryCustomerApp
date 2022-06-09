25c3dc755999916f6efbbc087ea46858c64c38e4
CyberMuffin <cybermuffinofficial@gmail.com>
2022-06-09 18:22:48 +0530

    aded react native elements, home screen and navoptions

--- ReactNativeDeliveryCustomerApp/App.tsx
+++ ReactNativeDeliveryCustomerApp/App.tsx
@@ -1,17 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
 import { Provider } from 'react-redux';
 import store from './app/store';
+import HomeScreen from './screens/HomeScreen';
 
 export default function App() {
 	return (
 		<Provider store={store}>
-			<View style={styles.container}>
-				<Text>Open up App.tsx to start working on your app!</Text>
-				<StatusBar style='auto' />
-			</View>
+			<HomeScreen />
 		</Provider>
 	);
+}
+
+{
+	/* <View style={styles.container}>
+<Text>Open up App.tsx to start working on your app!</Text>
+<StatusBar style='auto' />
+</View> */
 }
 
 const styles = StyleSheet.create({
